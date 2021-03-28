4708d4fa6acd49c67289ffed5d341ec7165e5b16
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2021-03-28 19:20:21 -0300

    importing font and declaring global fonts styles

--- dtmoney/src/styles/global.ts
+++ dtmoney/src/styles/global.ts
@@ -47,6 +47,15 @@ const GlobalStyle = createGlobalStyle`
     opacity: 0.6;
     cursor: not-allowed;
   }
+
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
+  }
 `;
 
 export default GlobalStyle;
