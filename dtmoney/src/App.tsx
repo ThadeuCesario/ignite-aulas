ff1dfed22b4097a943b2fd9b3999090c83dcf613
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2021-03-28 18:31:59 -0300

    working with styled components

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -1,7 +1,9 @@
+import styled from 'styled-components';
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello world!</h1>
+      <h1 className="title">Hello world!</h1>
     </div>
   );
 }
