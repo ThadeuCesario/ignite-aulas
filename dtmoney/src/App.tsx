f87e9da6a8d12230cc2c4befb976c952e1e58e56
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2021-03-28 21:09:01 -0300

    implemeting dashboard container

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -1,10 +1,12 @@
 import Header from './components/Header';
+import Dashboard from './components/Dashboard';
 import GlobalStyle from "./styles/global";
 
 function App() {
   return (
     <>
       <Header />
+      <Dashboard />
       <GlobalStyle />
     </>
   );
---
+++ dtmoney/src/components/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import { Container } from './styles';
+import Summary from '../Summary';
+
+function Dashboard() {
+  return (
+    <Container>
+      <Summary />
+    </Container>
+  );
+} 
+
+export default Dashboard;
