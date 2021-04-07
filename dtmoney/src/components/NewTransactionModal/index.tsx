157809886f2d9471aad69393641dadf8dee44c66
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2021-04-06 23:20:13 -0300

    applying styles modal

--- dtmoney/src/components/NewTransactionModal/index.tsx
+++ dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,5 @@
 import Modal from 'react-modal';
+import {Container} from './styles';
 
 interface NewTransactionModalProps {
     isNewTransactionModalOpen: boolean,
@@ -9,11 +10,26 @@ function NewTransactionModal(props: NewTransactionModalProps) {
     const {isNewTransactionModalOpen, handleToggleTransactionModalOpen} = props;
     return (
         <Modal 
-        isOpen={isNewTransactionModalOpen}
-        onRequestClose={handleToggleTransactionModalOpen}
-        ariaHideApp={false}
-      >
-        <h2>Cadastrar transação</h2>
+            isOpen={isNewTransactionModalOpen}
+            onRequestClose={handleToggleTransactionModalOpen}
+            overlayClassName="react-modal-overlay"
+            className="react-modal-content"
+            ariaHideApp={false}
+        >
+            <Container>
+                <h2>Cadastrar transação</h2>
+
+                <input placeholder="Título"/>
+
+                <input type="number" placeholder="Valor"/>
+
+                <input placeholder="Categoria"/>
+
+                <button type="submit">
+                    Cadastrar
+                </button>
+            </Container>
+
       </Modal>
     )
 }
