0e9ffac6a2f22fc97846ac6145abc72b093f8c01
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2021-04-12 22:23:51 -0300

    converting date and money using Intl

--- dtmoney/src/components/TransactionsTable/index.tsx
+++ dtmoney/src/components/TransactionsTable/index.tsx
@@ -39,9 +39,18 @@ function TransactionsTable() {
                             return (
                                 <tr key={id}>
                                     <td>{title}</td>
-                                    <td className={type}>{amount}</td>
+                                    <td className={type}>
+                                        {new Intl.NumberFormat('pt-BR', {
+                                            style: 'currency',
+                                            currency: 'BRL'
+                                        }).format(amount)}
+                                    </td>
                                     <td>{category}</td>
-                                    <td>{createdAt}</td>
+                                    <td>
+                                        {new Intl.DateTimeFormat('pt-BR').format(
+                                            new Date(createdAt)
+                                        )}
+                                    </td>
                                 </tr>
                             )
                         })
